152981d5f73e16603a1581b9ccfc8aa74fe2fcc4
leejunesun <dlwnstjs9592@naver.com>
2019-10-21 10:22:53 +0900

    201720970

---
+++ average.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+double aver-age(double in1,dounle in2)
+int main(void){
+
+	int sum;
+	printf("two mistake enter");
+	scanf("%d %d", &in1,&in2);
+	sum=in1+in2
+	avg=sum/2.0;
+return 0;
+}
+
+
